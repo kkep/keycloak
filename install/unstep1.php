844c5d44f6840dc3a5cbabf962412c66b4edcffc
A.Khayrullin <AGKhairullin@alabuga.ru>
2025-04-18 14:23:57 +0300

    Up

--- install/unstep1.php
+++ install/unstep1.php
@@ -1,15 +1,17 @@
-<form action="<?echo $APPLICATION->GetCurPage()?>">
-<?=bitrix_sessid_post()?>
-	<input type="hidden" name="lang" value="<?echo LANG?>">
-	<input type="hidden" name="id" value="keycloak">
-	<input type="hidden" name="uninstall" value="Y">
-	<input type="hidden" name="step" value="2">
+<!--<form action="--><?//echo $APPLICATION->GetCurPage()?><!--">-->
+<?php //=bitrix_sessid_post()?>
+<!--	<input type="hidden" name="lang" value="--><?php //=LANG?><!--">-->
+<!--	<input type="hidden" name="id" value="keycloak">-->
+<!--	<input type="hidden" name="uninstall" value="Y">-->
+<!--	<input type="hidden" name="step" value="2">-->
+<!---->
+<!--    --><?//echo CAdminMessage::ShowMessage(GetMessage("MOD_UNINST_WARN"))?>
+<!---->
+<!--    <p>-->
+<!--        --><?//echo GetMessage("MOD_UNINST_SAVE")?>
+<!--    </p>-->
+<!---->
+<!--    <input type="submit" name="inst" value="--><?//echo GetMessage("MOD_UNINST_DEL")?><!--">-->
+<!--</form>-->
+<p>Hello</p>
 
-    <?echo CAdminMessage::ShowMessage(GetMessage("MOD_UNINST_WARN"))?>
-
-    <p>
-        <?echo GetMessage("MOD_UNINST_SAVE")?>
-    </p>
-
-    <input type="submit" name="inst" value="<?echo GetMessage("MOD_UNINST_DEL")?>">
-</form>
