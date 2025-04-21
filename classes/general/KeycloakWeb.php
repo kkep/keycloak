04ca81640df79c7ce7a815cc364c0c253b72b748
A.Khayrullin <AGKhairullin@alabuga.ru>
2025-04-21 11:53:54 +0300

    Up

--- classes/general/KeycloakWeb.php
+++ classes/general/KeycloakWeb.php
@@ -944,7 +944,7 @@ class KeycloakWeb
 
     public static function onPageStart()
     {
-        if (true || COption::GetOptionString('keycloak', 'enabled', 'N') === 'N') {
+        if (COption::GetOptionString('keycloak', 'enabled', 'N') === 'N') {
             return;
         }
 
@@ -1002,7 +1002,7 @@ class KeycloakWeb
 
     public static function onBeforeProlog()
     {
-        if (true || COption::GetOptionString('keycloak', 'enabled', 'N') === 'N') {
+        if (COption::GetOptionString('keycloak', 'enabled', 'N') === 'N') {
             return;
         }
 
@@ -1017,7 +1017,7 @@ class KeycloakWeb
 
     public static function onAfterUserLogout($arParams)
     {
-        if (true || COption::GetOptionString('keycloak', 'enabled', 'N') === 'N') {
+        if (COption::GetOptionString('keycloak', 'enabled', 'N') === 'N') {
             return;
         }
 
