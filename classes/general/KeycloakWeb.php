86deea43ea7c90f254efd965350bed958dd45aaf
A.Khayrullin <AGKhairullin@alabuga.ru>
2025-04-21 12:20:06 +0300

    Up

--- classes/general/KeycloakWeb.php
+++ classes/general/KeycloakWeb.php
@@ -1024,8 +1024,6 @@ class KeycloakWeb
         if ($arParams['SUCCESS']) {
             $logoutUrl = static::instance()->getLogoutUrl();
             static::instance()->forgetToken();
-            echo $logoutUrl;
-            exit();
             header("Location: $logoutUrl");
         }
     }
