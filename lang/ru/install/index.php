1c0d68d476cfe8348fce596a4b4bc0c62e60c908
A.Khayrullin <AGKhairullin@alabuga.ru>
2025-04-30 11:17:37 +0300

    Up

--- lang/ru/install/index.php
+++ lang/ru/install/index.php
@@ -1,5 +1,7 @@
 <?
 // uses
+$MESS ['KEYCLOAK_MODULE_NAME'] = "Keycloak интеграция";
+$MESS ['KEYCLOAK_MODULE_DESC'] = "Модуль для работы с Keycloak.";
 $MESS ['KEYCLOAK_INSTALL_TITLE'] = "Установка модуля Keycloak";
 $MESS ['KEYCLOAK_UNINSTALL_TITLE'] = "Деинсталляция модуля Keycloak интеграции.";
 ?>
