9c0b6acb8bf6556b88525c92bd74a0c56afff873
A.Khayrullin <AGKhairullin@alabuga.ru>
2025-05-01 08:08:54 +0300

    Up

--- classes/general/KeycloakWeb.php
+++ classes/general/KeycloakWeb.php
@@ -125,7 +125,7 @@ class KeycloakWeb
         }
 
         if (is_null($this->callbackUrl)) {
-            $this->callbackUrl = 'https://' . $_SERVER['HTTP_HOST'] . '/' . $_SERVER['REQUEST_URI']; // COption::GetOptionString('keycloak', 'redirect_url', 'N');
+            $this->callbackUrl = COption::GetOptionString('keycloak', 'redirect_url');
         }
 
         if (is_null($this->redirectLogout)) {
