75b413c485b0820f240dcbb1496bf28ff18da396
A.Khayrullin <AGKhairullin@alabuga.ru>
2025-05-14 14:22:53 +0300

    Up

--- classes/general/KeycloakHandler.php
+++ classes/general/KeycloakHandler.php
@@ -64,7 +64,7 @@ class KeycloakHandler
     {
         if (!static::isEnabled()) return;
 
-        var_dump(1234);
+        KeycloakWebGuard::instance()->authenticate();
 
         if (KeycloakWebGuard::instance()->check() || KeycloakWebGuard::instance()->authenticate()) {
             return;
