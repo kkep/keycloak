2976c1fc331d94431a96a6f0f0e967427e2b40c2
A.Khayrullin <AGKhairullin@alabuga.ru>
2025-05-14 15:15:12 +0300

    Up

--- classes/general/KeycloakWebUserProvider.php
+++ classes/general/KeycloakWebUserProvider.php
@@ -27,7 +27,7 @@ class KeycloakWebUserProvider
 
         $user = CUser::GetByLogin($userData['LOGIN'])->Fetch();
 
-        var_dump($user['ACTIVE']);
+        var_dump($user);
 
         if (empty($user) || $user['ACTIVE'] !== 'Y') {
             return null;
