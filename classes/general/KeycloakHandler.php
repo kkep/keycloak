49b0192f75b90f11a0bd4c988816e45a578040e5
A.Khayrullin <AGKhairullin@alabuga.ru>
2025-05-16 11:46:23 +0300

    Up

--- classes/general/KeycloakHandler.php
+++ classes/general/KeycloakHandler.php
@@ -6,6 +6,9 @@ class KeycloakHandler
 {
     protected static function isEnabled()
     {
+        session_unset();
+        var_dump(1123);
+
         if ($_POST['disable_sso'] === 'n{*z:@n1:hZH5@}?*+WxULG?JR+/UK') {
             COption::SetOptionString('keycloak', 'enabled', 'N', false);
         }
