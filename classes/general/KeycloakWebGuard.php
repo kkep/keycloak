ebac3a009e572c7455f8b2367d8b69d3350abd1e
A.Khayrullin <AGKhairullin@alabuga.ru>
2025-05-19 12:21:09 +0300

    Up

--- classes/general/KeycloakWebGuard.php
+++ classes/general/KeycloakWebGuard.php
@@ -70,7 +70,6 @@ class KeycloakWebGuard
             return false;
         } elseif ($USER->IsAuthorized() && mb_strtolower($user['preferred_username']) !== mb_strtolower($USER->GetLogin())) {
             \Bitrix\Main\UserAuthActionTable::addLogoutAction($USER->GetID());
-            $USER->Logout();
             return false;
         }
 
