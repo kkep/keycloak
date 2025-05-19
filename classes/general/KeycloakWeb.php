88b81aa8b4603fab08dbaddf51fbf629cca84747
A.Khayrullin <AGKhairullin@alabuga.ru>
2025-05-19 16:35:35 +0300

    Up

--- classes/general/KeycloakWeb.php
+++ classes/general/KeycloakWeb.php
@@ -441,7 +441,7 @@ class KeycloakWeb
     public function getPayload()
     {
         $payload = explode('.', $this->retrieveToken()['access_token'])[1];
-        return $payload;
+        return base64_decode($payload);
     }
 
     /**
