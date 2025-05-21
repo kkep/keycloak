8dc2446936856f3ef3e1e9271287f91346b68017
A.Khayrullin <AGKhairullin@alabuga.ru>
2025-05-21 10:12:49 +0300

    Up

--- default_options.php
+++
@@ -1,8 +0,0 @@
-<?
-$keycloak_default_option = [
-    "enabled" => "N",
-    "add_user_when_auth" => "N",
-    "update_user_when_auth" => "N",
-    "disable_sso_secret_key" => "n{*z:@n1:hZH5@}?*+WxULG?JR+/UK"
-];
-?>
--- prolog.php
+++ prolog.php
@@ -1,4 +1,3 @@
 <?
 define("ADMIN_MODULE_NAME", "keycloak");
-define("ADMIN_MODULE_ICON", "<img src=\"/bitrix/images/keycloak/keycloak.gif\" width=\"48\" height=\"48\" border=\"0\" alt=\""."Keycloak"."\" title=\""."Keycloak"."\">");
 ?>
